bb5a650cb0633e8276be061e10b58fc051c97c6c
Fan Ling <troyprag816@gmail.com>
2019-03-23 18:08:25 -0400

    milestone_1

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	public float speed = 10f;
+
+    public void Update()
+    {
+        //camera drag
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 temp;
+            temp = transform.position;
+            temp -= Input.GetAxis("Mouse X") * speed * Time.deltaTime * transform.right;
+            temp -= Input.GetAxis("Mouse Y") * speed * Time.deltaTime * transform.up;
+            transform.position = temp;
+        }
+    }
+}
+	
---
+++ Assets/Scripts/CharacterMovment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMovment : MonoBehaviour {
+
+    public Camera cam;
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
